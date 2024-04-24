5e70645da9901799996be2c436b509195ecabde6
Stefano Bianda <stefanobianda@ik.me>
2024-04-24 11:03:41 +0200

    Spring Boot Initializer

---
+++ src/main/java/ch/sbsoft/coverletter/CoverletterApplication.java
@@ -0,0 +1,13 @@
+package ch.sbsoft.coverletter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoverletterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoverletterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/sbsoft/coverletter/CoverletterApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.sbsoft.coverletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoverletterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
