93aecbf14f84388ad471c60f8705b680628c8159
Stefano Bianda <stefanobianda@ik.me>
2024-04-30 08:52:06 +0200

    make running the controller class

--- src/main/java/ch/sbsoft/coverletter/controller/ApplicationController.java
+++ src/main/java/ch/sbsoft/coverletter/controller/ApplicationController.java
@@ -9,13 +9,13 @@ import ch.sbsoft.coverletter.specification.MappingPath;
 @Controller
 public class ApplicationController {
 
-//	// handler method to handle home page request
-//	@GetMapping(MappingPath.INDEX)
-//	public String index() {
-//		return MappingPage.INDEX;
-//	}
-//	@GetMapping("/")
-//	public String home() {
-//		return MappingPage.INDEX;
-//	}
+	// handler method to handle home page request
+	@GetMapping(MappingPath.INDEX)
+	public String index() {
+		return MappingPage.INDEX;
+	}
+	@GetMapping("/")
+	public String home() {
+		return MappingPage.INDEX;
+	}
 }
