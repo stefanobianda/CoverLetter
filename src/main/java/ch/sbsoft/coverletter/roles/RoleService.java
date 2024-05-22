e5eb5169bbd476cf0c4b7a50443b5c8843d5a267
Stefano Bianda <stefanobianda@ik.me>
2024-05-22 09:33:05 +0200

    make the test running pass

--- src/main/java/ch/sbsoft/coverletter/roles/RoleService.java
+++ src/main/java/ch/sbsoft/coverletter/roles/RoleService.java
@@ -2,16 +2,24 @@ package ch.sbsoft.coverletter.roles;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RoleService {
 
-	public Role findRoleByName(String string) {
-		// TODO Auto-generated method stub
-		return null;
+	@Autowired
+	private RoleRepository roleRepository;
+	
+	public Role findRoleByName(String roleName) {
+		Role role = roleRepository.findByName(roleName);
+		return role;
 	}
 
 	public List<Role> findAll() {
-		// TODO Auto-generated method stub
-		return null;
+		List<Role> roleList = roleRepository.findAll();
+		return roleList;
 	}
+	
 
 }
