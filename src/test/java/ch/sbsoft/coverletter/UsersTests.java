358b74d26ef7f077724839eb0b69e46f4fbc8626
Stefano Bianda <stefanobianda@ik.me>
2024-05-22 10:20:44 +0200

    additional test for IF UserDetails

--- src/test/java/ch/sbsoft/coverletter/UsersTests.java
+++ src/test/java/ch/sbsoft/coverletter/UsersTests.java
@@ -3,22 +3,15 @@ package ch.sbsoft.coverletter;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.fail;
-import static org.mockito.Mockito.when;
-
-import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import ch.sbsoft.coverletter.users.User;
-import ch.sbsoft.coverletter.users.UserRepository;
 import ch.sbsoft.coverletter.users.UserService;
 
 @SpringBootTest
@@ -35,11 +28,12 @@ class UsersTests {
 
 	}
 
-		@Test
+	@Test
 	void testFindUserByUsername() {
 		User user = userService.findUserByUsername("admin@test.com");
 		assert(user.getUsername().equalsIgnoreCase("admin@test.com"));
-
+		assertEquals("Test", user.getFirstName());
+		assertEquals("Admin", user.getLastName());
 	}
 
 	@Test
@@ -53,5 +47,21 @@ class UsersTests {
 		}
 	}
 
+	@Test
+	void testLoadUserByUsername() {
+		UserDetails user = userService.findUserByUsername("admin@test.com");
+		assert(user instanceof UserDetails);
+		assert(user.getUsername().equalsIgnoreCase("admin@test.com"));
+	}
 	
+	@Test
+	void testLoadUserByUsernameNotFound() {
+		try {
+			userService.findUserByUsername("nouser@test.com");
+			fail("Expected exception was not thrown");
+		} catch (UsernameNotFoundException e) {
+			// Assert that the exception message is correct
+			assertEquals("Could not find user", e.getMessage());
+		}
+	}
 }
