9f14e0d5b4d83deb9b6a841bc2030d4ae4c188d2
Stefano Bianda <stefanobianda@ik.me>
2024-05-22 09:49:23 +0200

    test are pass

--- src/main/java/ch/sbsoft/coverletter/users/UserService.java
+++ src/main/java/ch/sbsoft/coverletter/users/UserService.java
@@ -1,10 +1,30 @@
 package ch.sbsoft.coverletter.users;
 
-public class UserService {
+import java.util.Optional;
 
-	public User findUserByUsername(String string) {
-		// TODO Auto-generated method stub
-		return null;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	public User findUserByUsername(String username) {
+		Optional<User> user = userRepository.findByUsername(username);
+		if (!user.isPresent()) {
+			throw new UsernameNotFoundException("Could not find user");
+		}
+		return user.get();
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		return findUserByUsername(username);
 	}
 
 }
