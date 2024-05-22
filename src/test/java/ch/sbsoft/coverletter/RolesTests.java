2e496fb8e3ea02388ebd51f2ca010fd38746e776
Stefano Bianda <stefanobianda@ik.me>
2024-05-22 09:32:50 +0200

    setup correct the test

--- src/test/java/ch/sbsoft/coverletter/RolesTests.java
+++ src/test/java/ch/sbsoft/coverletter/RolesTests.java
@@ -1,5 +1,6 @@
 package ch.sbsoft.coverletter;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
@@ -11,31 +12,32 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import ch.sbsoft.coverletter.roles.Role;
 import ch.sbsoft.coverletter.roles.RoleRepository;
 import ch.sbsoft.coverletter.roles.RoleService;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
 class RolesTests {
 
-	@Mock
-	private RoleRepository roleRepositoryMock;
-
-	@InjectMocks
+	@Autowired
 	private RoleService roleService;
 
 	
 	@Test
 	void testValidObjects() {
 		assertNotNull(roleService);
-		assertNotNull(roleRepositoryMock);
 
 	}
 
 	@Test
 	void testFindRoleByName() {
-		when(roleRepositoryMock.findByName("ROLE_USER")).thenReturn(new Role(1l,"ROLE_USER",null));
 		Role role = roleService.findRoleByName("ROLE_USER");
 		assert(role.getName().equals("ROLE_USER"));
 
@@ -43,16 +45,14 @@ class RolesTests {
 
 	@Test
 	void testFindRoleByNameNotFound() {
-		when(roleRepositoryMock.findByName("ROLE_DOES_NOT_EXIST")).thenReturn(null);
 		Role role = roleService.findRoleByName("ROLE_DOES_NOT_EXIST");
 		assertNull(role);
 	}
 
 	@Test
 	void testFindAllRoles() {
-		when(roleRepositoryMock.findAll()).thenReturn(null);
 		List<Role> roleList = roleService.findAll();
-		assert(roleList.size() >= 2);
+		assertEquals(2, roleList.size());
 	}
 
 	
